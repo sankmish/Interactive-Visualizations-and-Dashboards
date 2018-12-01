e46ac436e7f2ce6594b9efd5162746c67b4b17c5
sankmish <mishra.sancho@gmail.com>
2018-12-01 16:32:08 -0500

    Completed Interactive Dashboard
    Fixed parsing errors on data. Fixed slice function.  Added Sort function to Flask app instead of app.js. Included procfile and changed procfile for heroku deployment.

--- StarterCode/Belly_Button_Biodiversity/static/js/app.js
+++ StarterCode/Belly_Button_Biodiversity/static/js/app.js
@@ -16,13 +16,13 @@ function buildMetadata(sample) {
 
 
 
-  // Why aren't my console statements showing?  
-  var url = "/metadata/" + sample;
-  console.log(url);
+  var url = `/metadata/${sample}`;
+  console.log(`${sample}`);
+console.log(url);
 
   d3.json(url).then(function(response) {
-  console.log(response);
-    var data = [response];
+    var data = response;
+    console.log(data);
     var sampleMetadata = d3.select("#sample-metadata");
     sampleMetadata.html("");
   
@@ -30,12 +30,9 @@ function buildMetadata(sample) {
       var instance = sampleMetadata.append("p");
         instance.text(key + ": " + value);
 
-        //Is this the correct object.entries call on data for a single dictionary with multiple key value pairs?
     });
       
   });
-
-  //Is this code segment wrong? Nnot sure why it is not working. 
 
 }
 
@@ -44,69 +41,59 @@ function buildMetadata(sample) {
 
 
 function buildCharts(sample) {
-// @TODO: Use `d3.json` to fetch the sample data for the plots
 
-    // @TODO: Build a Bubble Chart using the sample data
-
-    // @TODO: Build a Pie Chart
-    // HINT: You will need to use slice() to grab the top 10 sample_values,
-    // otu_ids, and labels (10 each).
-
-  var url = "/samples/" + sample;
+  var url = `/samples/${sample}`;
   console.log(url);
 
 
   d3.json(url).then(function(response) {
-
-    console.log(response);
-    var data = [response];
-    console.log(data);
-
-    //Is this the right way to call the elements in the data? data.otu_ids, response.otu_ids, or list comprehension?
+    var data = response;
+    console.log("data " + data.otu_ids);
     var trace1 = {
       x: data.otu_ids,
       y: data.sample_values,
       mode: "markers",
-      type: "scatter", 
-      marker: {size : data.sample_values},
-      color: data.otu_ids,
+      marker: {
+        size : data.sample_values,
+        color: data.otu_ids
+      },
       text: data.otu_labels
     }
 
     var layout = {
-      xaxis: { range: [0, 3500] },
-      yaxis: { range: [0, 250]  },
+      xaxis: { 
+        range: [0, 3500],
+        title: "OTU ID"
+      },
+      yaxis: { 
+        range: [0, 250],
+        title: "Sample Values"
+        },
       title: 'OTU ID Vs. Sample Values'
     }
 
-    //How do I get the plot to place in this div ^-
-    Plotly.newPlot("bubble", data, layout);
+    Plotly.newPlot("bubble", [trace1], layout);
 
 
-    //If you sort this will the other values move with it?
-    data.sample_values.sort(function compareFunction(firstNum, secondNum){
-      return secondNum - firstNum;
-    });
-
-    data.slice(0,10);
 
     var pieChartColors = ['rgb(0, 57, 230)', 'rgb(255, 102, 0)', 'rgb(0, 179, 60)', 'rgb(153, 0, 0)', 'rgb(102, 102, 255)', 'rgb(116, 37, 77)',
                    'rgb(204, 102, 255)', 'rgb(102, 102, 153)', 'rgb(153, 153, 77)', 'rgb(102, 204, 255)' ]
 
     var trace2 = {
-      values: data.sample_values,
-      labels: data.otu_ids,
-      hoverinfo: data.otu_labels,
+      values: data.sample_values.slice(0,10),
+      labels: data.otu_ids.slice(0,10),
+      hoverinfo: data.otu_labels.slice(0,10),
       type: 'pie',
-      marker: {colors: pieChartColors}
+      marker: {
+        colors: pieChartColors
+      }
     };
 
     var layout2 = {
       title: 'OTU ID Vs. Sample Values'
     }
 
-    //How do I get the plot to place in this div ^-
-    Plotly.newPlot("pie", trace2, layout2);
+    Plotly.newPlot("pie", [trace2], layout2);
   });
 
 }
@@ -114,10 +101,8 @@ function buildCharts(sample) {
 
 
 function init() {
-  // Grab a reference to the dropdown select element
   var selector = d3.select("#selDataset");
-
-  // Use the list of sample names to populate the select options
+  
   d3.json("/names").then((sampleNames) => {
     sampleNames.forEach((sample) => {
       selector
